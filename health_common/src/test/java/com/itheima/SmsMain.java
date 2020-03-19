773b379ddcf715dcb8859b5c3f289f6965441f7b
EthanYu6 <1037916932@qq.com>
2020-03-19 19:08:00 +0800

    发送短信1

--- health_common/src/test/java/com/itheima/SmsMain.java
+++ health_common/src/test/java/com/itheima/SmsMain.java
@@ -11,6 +11,6 @@ import com.itheima.health.utils.SMSUtils;
 
 public class SmsMain {
     public static void main(String[] args) throws Exception{
-        SMSUtils.sendShortMessage("SMS_169899968","18722089939","1234");
+        SMSUtils.sendShortMessage("SMS_17868648","18678675464","1234");
     }
 }
